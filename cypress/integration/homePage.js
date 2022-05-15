df55490fac6e42ce2dee462b787dc6d3bb359012
Dawid Podsiadły <dawid.podsiadly@interia.pl>
2022-05-15 22:27:30 +0200

    form complete script

---
+++ cypress/integration/homePage.js
@@ -0,0 +1,18 @@
+/// <reference types = "cypress" />
+import 'cypress-file-upload';
+
+describe("E2E - Home Page", () => {
+    it("Completes the form", () => {
+        cy.visit("/")
+        cy.get('[title="Contact Us"]').click()
+        cy.get("#message").type("Random message for test.")
+        cy.get("#email").type("test@gmail.com")
+        cy.get("#id_order").type("874237")
+        cy.get("#id_contact").select("Customer service")
+
+        const fixtureFile = 'test.png';
+        cy.get("#fileUpload").attachFile(fixtureFile);
+
+        cy.get("span").parents("#submitMessage").click()
+    })
+})
--- cypress/integration/homePageOpen.js
+++
@@ -1,7 +0,0 @@
-/// <reference types = "cypress" />
-
-describe("E2E - Home Page", () => {
-    it("Should open home page - automationpractice", () => {
-        cy.visit("/")
-    })
-})
